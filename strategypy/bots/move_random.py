0392b6bd72e01404a603d5cc254cf8354163d18d
Davide Ceretti <davide.ceretti@hogarthww.com>
2014-08-01 14:49:28 +0100

    Prettify move_randon

--- strategypy/bots/move_random.py
+++ strategypy/bots/move_random.py
@@ -2,14 +2,10 @@ import random
 from api import BaseBot
 
 DIRECTIONS = ('up', 'down', 'left', 'right')
-_shared_dict = {'_direction': random.choice(DIRECTIONS)}
 
 
 class Bot(BaseBot):
-    def __init__(self, *args, **kwargs):
-        future = super(Bot, self).__init__(*args, **kwargs)
-        self.__dict__.update(**_shared_dict)
-        return future
+    direction = random.choice(DIRECTIONS)
 
     @property
     def happy_condition(self):
@@ -21,12 +17,12 @@ class Bot(BaseBot):
             'left': x == 0,
             'right': x == X - 1,
         }
-        return conditions[self._direction]
+        return conditions[self.direction]
 
     def action(self):
         if self.happy_condition:
             return
         moved = not self.position == self.previous_position
         chosen_move = random.choice(DIRECTIONS)
-        where = self._direction if moved else chosen_move
+        where = self.direction if moved else chosen_move
         return 'move {}'.format(where)
