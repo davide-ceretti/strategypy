346d2e90dfdcafd08e140f11aace18c2f0414aa5
Davide Ceretti <dav.ceretti@gmail.com>
2014-08-17 17:50:53 +0100

    Add more unit tests

--- strategypy/tests/test_integration.py
+++ strategypy/tests/test_integration.py
@@ -5,6 +5,7 @@ by using some custom bots.
 
 import unittest
 import json
+import itertools
 
 from mock import patch
 
@@ -29,8 +30,8 @@ class TestGameCanRun(unittest.TestCase):
         self.assertIsNotNone(result)
 
 
-@patch.object(settings, 'UNITS', 3)
-@patch.object(settings, 'MAX_TURNS', 5)
+@patch.object(settings, 'UNITS', 1)
+@patch.object(settings, 'MAX_TURNS', 3)
 @patch.object(settings, 'GRID_SIZE', (10, 10))
 class TestGameOutput(unittest.TestCase):
     def test_player_info(self):
@@ -67,7 +68,7 @@ class TestGameOutput(unittest.TestCase):
 @patch.object(settings, 'GRID_SIZE', (10, 10))
 @patch.object(Unit, 'get_random_location')
 class TestMovement(unittest.TestCase):
-    def assert_moved(self, game, player_pk, direction, unit_pk='0'):
+    def assertUnitMoved(self, game, player_pk, direction, unit_pk='0'):
         """
         Assert that the unit moved in the given direction in their
         first action
@@ -91,7 +92,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveup')
 
-        self.assert_moved(result, '1', 'up')
+        self.assertUnitMoved(result, '1', 'up')
 
     def test_cant_move_up_occupied(self, get_random_location):
         locations = [(5, 4), (5, 5)]
@@ -99,7 +100,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveup')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_cant_move_up_on_border(self, get_random_location):
         locations = [(5, 5), (6, 0)]
@@ -107,7 +108,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveup')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_can_move_down(self, get_random_location):
         locations = [(0, 0), (5, 5)]
@@ -115,7 +116,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_movedown')
 
-        self.assert_moved(result, '1', 'down')
+        self.assertUnitMoved(result, '1', 'down')
 
     def test_cant_move_down_occupied(self, get_random_location):
         locations = [(5, 6), (5, 5)]
@@ -123,7 +124,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_movedown')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_cant_move_down_on_border(self, get_random_location):
         locations = [(5, 5), (6, 9)]
@@ -131,7 +132,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_movedown')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_can_move_left(self, get_random_location):
         locations = [(0, 0), (5, 5)]
@@ -139,7 +140,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveleft')
 
-        self.assert_moved(result, '1', 'left')
+        self.assertUnitMoved(result, '1', 'left')
 
     def test_cant_move_left_occupied(self, get_random_location):
         locations = [(5, 5), (6, 5)]
@@ -147,7 +148,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveleft')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_cant_move_left_border(self, get_random_location):
         locations = [(5, 5), (0, 9)]
@@ -155,7 +156,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveleft')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_can_move_right(self, get_random_location):
         locations = [(0, 0), (5, 5)]
@@ -163,7 +164,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveright')
 
-        self.assert_moved(result, '1', 'right')
+        self.assertUnitMoved(result, '1', 'right')
 
     def test_cant_move_right_occupied(self, get_random_location):
         locations = [(6, 5), (5, 5)]
@@ -171,7 +172,7 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveright')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
 
     def test_cant_move_right_border(self, get_random_location):
         locations = [(5, 5), (9, 1)]
@@ -179,4 +180,72 @@ class TestMovement(unittest.TestCase):
 
         result = play_game('unittest_static', 'unittest_moveright')
 
-        self.assert_moved(result, '1', None)
+        self.assertUnitMoved(result, '1', None)
+
+
+@patch.object(settings, 'UNITS', 2)
+@patch.object(settings, 'MAX_TURNS', 10)
+@patch.object(settings, 'GRID_SIZE', (2, 6))
+@patch.object(Unit, 'get_random_location')
+class TestKilling(unittest.TestCase):
+    locations = (
+        (0, 0), (0, 3),  # Player zero
+        (0, 5), (1, 5),  # Player one
+    )
+    """
+    The game should look like this. Read horizontally
+      0   1   2   3   4
+    ---------------------
+    |0 X|0 X|0 X|0 X|X X|
+    |X X|X X|X X|X X|1 1|
+    |X X|X X|X X|1 1|X X|
+    |0 X|X X|1 1|X X|X X|
+    |X X|1 1|1 1|1 1|1 1|
+    |1 1|X X|X X|X X|X X|
+    ---------------------
+    """
+    def test_move_up_is_winner(self, get_random_location):
+        get_random_location.side_effect = self.locations
+
+        result = play_game('unittest_static', 'unittest_moveup')
+
+        winner = result['winner']
+        self.assertEqual(winner, 1)
+
+    def test_static_units_are_killed(self, get_random_location):
+        get_random_location.side_effect = self.locations
+
+        result = play_game('unittest_static', 'unittest_moveup')
+
+        # Player zero has two units at frame zero
+        units = result['frames'][0]['0'].keys()
+        self.assertEqual(len(units), 2)
+
+        # Player zero has one unit at frame one
+        units = result['frames'][1]['0'].keys()
+        self.assertEqual(len(units), 1)
+
+        # Player zero loses at frame four
+        players = result['frames'][4].keys()
+        self.assertNotIn('0', players)
+
+
+@patch.object(settings, 'UNITS', 4)
+@patch.object(settings, 'MAX_TURNS', 1)
+@patch.object(settings, 'GRID_SIZE', (3, 4))
+class TestSpawning(unittest.TestCase):
+    def test_units_are_spawned_in_different_cells(self):
+        """
+        Randomly spawns 12 units in 12 cells
+        """
+        result = play_game(
+            'unittest_static', 'unittest_static', 'unittest_static'
+        )
+
+        units = itertools.chain(
+            *[unit.values() for unit in result['frames'][0].values()]
+        )
+        units = sorted(list(units))
+
+        expected = [[x, y] for x in xrange(3) for y in xrange(4)]
+        self.assertListEqual(units, expected)
