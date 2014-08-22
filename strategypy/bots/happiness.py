a2c434363a6f365138b2323aacad4062d942671f
Robert Boston <robert.t.boston@gmail.com>
2014-08-22 09:18:41 +0100

    enemies make me happy

--- strategypy/bots/happiness.py
+++ strategypy/bots/happiness.py
@@ -26,17 +26,25 @@ class Bot(HappinessBaseBot):
     
 
     def calc_happiness(self, friend_dist, enemy_dist):
-        # row is distance, value is happiness based on num bots there
-
+        
         happiness = 0. #+ random.random()/100.
 
+        # occupied cells are unhappy
         if friend_dist[0] or enemy_dist[0]:
             return happiness
 
+        # friends make me happy
         for distance in range(1, 100):
 
             max_per_shell = 4 * distance
             shell_occupancy = (friend_dist[distance] * 1. / max_per_shell)
             happiness += min(shell_occupancy, 0.74) / distance
 
+        # enemiess make me happy, but not as much
+        for distance in range(1, 100):
+
+            max_per_shell = 4 * distance
+            shell_occupancy = (enemy_dist[distance] * 1. / max_per_shell)
+            happiness += 0.5 * min(shell_occupancy, 0.74) / distance
+        
         return happiness
