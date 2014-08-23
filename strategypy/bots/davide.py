725d9a307365ce26a51d9df17c3290a3dffe138d
Davide Ceretti <dav.ceretti@gmail.com>
2014-08-23 10:38:11 +0100

    Cleanup in davide bot

--- strategypy/bots/davide.py
+++ strategypy/bots/davide.py
@@ -1,7 +1,32 @@
-import random
 import operator
 
 from api import BaseBot
+
+
+# Utilities
+
+def max_from_dict(dictionary):
+    result = max(
+        dictionary.iteritems(),
+        key=operator.itemgetter(1)
+    )[0]
+    return result
+
+
+def get_me_closer_to(my_position, their_position):
+    my_x, my_y = my_position
+    their_x, their_y = their_position
+    dx = my_x - their_x
+    dy = my_y - their_y
+
+    result = {
+        'move up': float(abs(dx) <= abs(dy) and dy >= 0),
+        'move down': float(abs(dx) <= abs(dy) and dy < 0),
+        'move left': float(abs(dx) > abs(dy) and dx >= 0),
+        'move right': float(abs(dx) > abs(dy) and dx < 0),
+        None: 0.0
+    }
+    return result
 
 
 class Bot(BaseBot):
@@ -14,9 +39,11 @@ class Bot(BaseBot):
     }
 
     rules = {
-        'be_able_to_move': 0.1,
-        'risk_of_dieing': 0.01,
-        'closer_to_central_mass': 0.001,
+        'be_able_to_move': 100.0,
+        'risk_of_dieing': 10.0,
+        'outnumber_isolated_enemies': 1.2,
+        'closer_to_central_mass': 1.0,
+        'find_isolated_targets': 0.0,  # Unused
     }
 
     def action(self, ctx):
@@ -29,11 +56,7 @@ class Bot(BaseBot):
             for action in self.actions
         }
 
-        chosen = max(
-            weighted_actions.iteritems(),
-            key=operator.itemgetter(1)
-        )[0]
-        return chosen
+        return max_from_dict(weighted_actions)
 
     # UTILS
 
@@ -63,24 +86,71 @@ class Bot(BaseBot):
     def closer_to_central_mass(self, ctx):
         board = ctx['current_data']
         pk = ctx['player_pk']
-        x, y = ctx['position']
+        my_position = ctx['position']
         allies = board[pk].values()
         n = len(allies)
-        avg_x = sum(x for x, _ in allies)/n
-        avg_y = sum(y for _, y in allies)/n
-        dx = x - avg_x
-        dy = y - avg_y
-        # TODO: Optimize
-        result = {
-            'move up': float(abs(dx) <= abs(dy) and dy >= 0),
-            'move down': float(abs(dx) <= abs(dy) and dy < 0),
-            'move left': float(abs(dx) > abs(dy) and dx >= 0),
-            'move right': float(abs(dx) > abs(dy) and dx < 0),
-            None: 0.0
+        avg_position = (
+            sum(x for x, _ in allies)/n,
+            sum(y for _, y in allies)/n
+        )
+
+        result = get_me_closer_to(my_position, avg_position)
+        return result
+
+    def outnumber_isolated_enemies(self, ctx):
+        board = ctx['current_data']
+        pk = ctx['player_pk']
+        my_position = ctx['position']
+        x, y = my_position
+        allies = board[pk].values()
+        enemies = [v.values() for k, v in board.iteritems() if k != pk][0]
+        close_allies = sum(
+            1
+            for ax, ay in allies
+            if abs(x-ax) <= 3 and abs(y-ay) <= 3
+        )
+        close_enemies = [
+            (ex, ey)
+            for ex, ey in enemies
+            if abs(x-ex) <= 6 and abs(y-ey) <= 6
+        ]
+
+        n = len(close_enemies)
+        if not n:
+            return {k: 1.0 for k in self.actions.keys()}
+
+        avg_position = (
+            sum(x for x, _ in close_enemies)/n,
+            sum(y for _, y in close_enemies)/n
+        )
+
+        result = get_me_closer_to(my_position, avg_position)
+
+        if close_allies >= len(close_enemies):
+            return result
+        else:
+            return {k: 1.0 - v for k, v in result.iteritems()}
+
+    def find_isolated_targets(self, ctx):
+        board = ctx['current_data']
+        pk = ctx['player_pk']
+        my_position = ctx['position']
+        enemies = [v.values() for k, v in board.iteritems() if k != pk][0]
+        n = len(enemies)
+        avg_enemies_x = sum(x for x, _ in enemies)/n
+        avg_enemies_y = sum(y for _, y in enemies)/n
+        distances_from_avg = {
+            (ex, ey): abs(ex - avg_enemies_x) + abs(ey - avg_enemies_y)
+            for ex, ey in enemies
         }
+
+        target_position = max_from_dict(distances_from_avg)
+        result = get_me_closer_to(my_position, target_position)
+
         return result
 
     def risk_of_dieing(self, ctx):
+        # TODO: Take into consideration ally positions
         board = ctx['current_data']
         pk = ctx['player_pk']
         x, y = ctx['position']
