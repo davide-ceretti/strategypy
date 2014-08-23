5bdd8bff543cea4382593e4096e53ddd41143a04
Davide Ceretti <dav.ceretti@gmail.com>
2014-08-23 15:21:50 +0100

    Remove typo

--- play-pygame.sh
+++ play-pygame.sh
@@ -1,3 +1,2 @@
 # Uses https://github.com/davide-ceretti/strategypy-pygame-client and expect it to be cloned in ../
-https://github.com/davide-ceretti/strategypy-pygame-client
 ./play.sh | python ../strategypy-pygame-client/strategypy-pygame-client/main.py
