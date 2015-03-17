e739663a1ba23838e09d810ac9e1e0d7b5dcd681
Raúl Cumplido <raulcumplido@gmail.com>
2015-03-17 13:21:02 +0000

    Add consolefe.py python3 support

--- strategypy/consolefe.py
+++ strategypy/consolefe.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
 import json
 import sys
 import copy
 from optparse import OptionParser
+
+import six
 
 
 def print_frames(output_dict):
@@ -12,7 +15,7 @@ def print_frames(output_dict):
         for i in range(grid_size[0])
     ]
 
-    print '-' * (grid_size[1] + 2)
+    print('-' * (grid_size[1] + 2))
 
     for frame in output_dict['frames']:
         grid = copy.deepcopy(empty_grid)
@@ -22,8 +25,8 @@ def print_frames(output_dict):
                 grid[x][y] = player_pk
 
         for row in grid:
-            print '|' + ''.join(row) + '|'
-        print '-' * (grid_size[1] + 2)
+            print('|' + ''.join(row) + '|')
+        print('-' * (grid_size[1] + 2))
 
 
 def print_summary(output_dict):
@@ -36,27 +39,27 @@ def print_summary(output_dict):
     initial_frame = output_dict['frames'][0]
     last_frame = output_dict['frames'][-1]
 
-    initial_count = sum((len(x) for x in initial_frame.itervalues()))
-    final_count = sum((len(x) for x in last_frame.itervalues()))
+    initial_count = sum((len(x) for x in six.itervalues(initial_frame)))
+    final_count = sum((len(x) for x in six.itervalues(last_frame)))
 
     winner = None if winner is None else players[str(winner)]
     if winner is None:
-        print 'No player won and the game ended in {} turns'.format(turns)
+        print('No player won and the game ended in {} turns'.format(turns))
     else:
-        print 'Player {} won in {} turns'.format(winner, turns)
-    print 'Initial unit count: {}'.format(initial_count)
-    print 'Final unit count: {}'.format(final_count)
+        print('Player {} won in {} turns'.format(winner, turns))
+    print('Initial unit count: {}'.format(initial_count))
+    print('Final unit count: {}'.format(final_count))
 
     for player in all_players.values():
-        print 'Player {} killed: '.format(player['name']),
+        print('Player {} killed: '.format(player['name']), end="")
         for killed_player, num_times in player['has_killed'].items():
-            print '{} x {}, '.format(killed_player, num_times),
-        print
+            print('{} x {}, '.format(killed_player, num_times), end="")
+        print()
     for player in all_players.values():
-        print 'Player {} was killed by: '.format(player['name']),
+        print('Player {} was killed by: '.format(player['name']), end="")
         for killed_player, num_times in player['was_killed_by'].items():
-            print '{} x {}, '.format(killed_player, num_times),
-        print
+            print('{} x {}, '.format(killed_player, num_times), end="")
+        print()
 
 
 if __name__ == "__main__":
